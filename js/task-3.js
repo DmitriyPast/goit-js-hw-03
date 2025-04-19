09cc3dff4f0e7f38c48e6468a1bda45587c01bbb
your_name <pastushenkodmitry2003@gmail.com>
2025-04-19 23:07:14 +0300

    HW3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
-function checkForSpam(message) {
-    const banWords = ["spam", "sale"];
-    return message.toLowerCase().includes(banWords[0]) || message.toLowerCase().includes(banWords[1]);
+function filterArray(numbers, value) {
+    const resArr = [];
+    for (const num of numbers) {
+        num > value ? resArr.push(num) : {};
+    }
+    return resArr;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
